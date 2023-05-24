4b67b86781924f8fa0db0112858245d6e6357593
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-05-25 03:33:03 +0900

    add planet and h1

--- main.js
+++ main.js
@@ -30,22 +30,48 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     document.body.appendChild(renderer.domElement);
+    // document.querySelector('.gravel').appendChild(renderer.domElement);
+    // document.querySelector('.rocks').appendChild(renderer.domElement);
     renderer.render(scene, camara);
     
     controls = new OrbitControls(camara , renderer.domElement);
     controls.update();
     // デクスチャを追加
     let textures = new THREE.TextureLoader().load("./textures/earth.jpg");
+    
+    let gravelTextures = new THREE.TextureLoader().load("./textures/gravel.jpg");
+
+    let rocksTextures = new THREE.TextureLoader().load("./textures/rocks.jpg");
     // 形を生成・物体の形状を表す物（球体・立方体・）
     let ballGeo = new THREE.SphereGeometry(100, 64, 32);
+    let gravelGeo = new THREE.SphereGeometry(100, 64, 32);
+    let rocksGeo = new THREE.SphereGeometry(100, 64, 32);
+
     // 柄・材質を設定
     let ballMaterial = new THREE.MeshPhysicalMaterial({
         map: textures
+    });
+
+    let gravelMaterial = new THREE.MeshPhysicalMaterial({
+        map: gravelTextures
+    });
+
+    let rocksMaterial = new THREE.MeshPhysicalMaterial({
+        map: rocksTextures
     });
     // メッシュ ＝＞ ジオメトリ＋マテリアル
     // メッシュ化
     let ballMesh = new THREE.Mesh(ballGeo, ballMaterial);
     scene.add(ballMesh);
+    
+    
+    let gravelMesh = new THREE.Mesh(gravelGeo, gravelMaterial);
+    gravelMesh.position.set(250,-100,10);
+    scene.add(gravelMesh);
+
+    let rocksMesh = new THREE.Mesh(rocksGeo, rocksMaterial);
+    rocksMesh.position.set(-250,-100,10);
+    scene.add(rocksMesh);
     // 並行光源の追加
     let directionalLight = new THREE.DirectionalLight(0xffffff , 2);
     directionalLight.position.set(1,1,1);
